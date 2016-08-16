d7b23f94cf9a87adc96998f42e1616f3c7f190e6
Abruzzi <abruzzi.hraig@gmail.com>
2016-08-16 12:42:48 +1000

    update(dayone): update parts of function in dayone class

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  JOURNAL_PATH: '/Users/abruzzi/Library/Group Containers/5U8NS4GX82.dayoneapp2/Data/Documents/DayOne.sqlite'
+  JOURNAL_PATH: '/Users/abruzzi/Library/Group Containers/5U8NS4GX82.dayoneapp2/Data/Documents/DayOne.sqlite',
+  LOCAL_REPO_PATH: '/Users/abruzzi/Public/road-for-revenge'
 };
--- dayone.js
+++ dayone.js
@@ -1,32 +1,81 @@
+var cfg = require('./config');
+
+/**
+ * convert unix timestamp to milliseconds could handle by JS
+ */
+const function timestampConvert(timestamp) {
+  return new Date(timestamp + 978307200) * 1000);
+}
+
 class DayOne {
   constructor(db) {
     this.db = db;
   }
 
-  getJournals() {
-    this.db.getAll('select ZTEXT, ZCREATIONDATE from ZENTRY').then(data => {
-      var results = data.map((val) => {
-        var time = (val.ZCREATIONDATE + 978307200) * 1000; // convert unix timestamp to milliseconds could handle by JS
+  getJournals(query) {
+    var journals = journals || null;
 
+    if (journals) return Promise.resolve(journals);
+
+    return this.db.getAll(query).then(data => {
+      journals = data.map((val) => {
         return {
-          createDate: new Date(time),
+          createDate: timestampConvert(val.ZCREATIONDATE),
           text: val.ZTEXT
         };
       });
-
-      console.info(results);
     }, (err) => {
       console.error(err);
     });
   }
 
-  writeToGithub() {
+  extractAllTitles(journals) {
+    return journals.map(val => {
+      // TODO extract title
+      return val.text.match('###');
+    });
   }
 
-  writeToHexo() {
+  writeToLocalRepo(journals) {
+    var titles = extractAllTitles(journals);
+
+    // Find if a journal is existed
+    // fs.read(cfg.LOCAL_REPO_PATH, ...)
+    //
+    //
+    // if it is existed, then update it, else create new one
+  }
+
+  deployToRemoteRepo() {
+    // Use command line to deploy the repo
+  }
+
+  writeToLocalHexo(journals) {
+  }
+
+  deployToRemoteHexo() {
   }
 
   createIssue() {
+  }
+
+  updateAllJournals() {
+    var query = 'select ZTEXT, ZCREATIONDATE from ZENTRY';
+
+    this.getJournals(query).then((journals) => {
+      this.writeToLocalRepo(journals);
+      this.writeToLocalHexo(journals);
+    }, console.error);
+  }
+
+  updateRecentJournals(lastModifiedTime) {
+    // TODO filter with modified time
+    var query = 'select ZTEXT, ZCREATIONDATE from ZENTRY';
+
+    this.getJournals(query).then((journals) => {
+      this.writeToLocalRepo(journals);
+      this.writeToLocalHexo(journals);
+    }, console.error);
   }
 }
 
